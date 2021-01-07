99721819145b16d706fa6488eebbfcac7ff37f70
jm-upmd <43405929+jm-upmd@users.noreply.github.com>
2021-01-07 17:34:26 +0100

    Update JuegoAhorcado.java

--- JuegoAhorcado2/src/juegoahorcado/JuegoAhorcado.java
+++ JuegoAhorcado2/src/juegoahorcado/JuegoAhorcado.java
@@ -86,7 +86,8 @@ public class JuegoAhorcado {
 	static String palabraADescubrir;  // Palabra a descubrir.
 		
 	// Palabra en construcción. Array donde se irá construyendo la palabra a medida que el usuario da letras.
-	static char[] palapraEnConstruccion; 
+	static char[] palabraEnConstruccion; 
+	             
 	
 	// Colección para almacenar las letras ya usadas y mostrarlas en la partida.
 	// Usamos una TreeSet porque no almacena duplicados y además ordena los elementos.
@@ -94,45 +95,28 @@ public class JuegoAhorcado {
 	// Ojo, por defecto no ordena bien la 'ñ' . Habría que usar un objeto Comparator para 
 	// establecer el orden correcto (eso lo veremos más adelante)
 	
-	static TreeSet<Character> letrasUtilizadas;
+	static TreeSet<Character> letrasUtilizadas =  new TreeSet<>(); 
 	
 	static int contFallos = 0;  // Contador de fallos. Se incrementa por cada letra suministrada que no existenta en la palabra a descubrir.
 	static int contTiradas = 0; // Contador del número de tiradas (letras suministradas) en la partida.
-	static int aciertos; 	    // Número de ocurrencias de la letra suministrada por el usuario en  la palabra a descubrir.
-	
+	static boolean acierto; // Indica si una jugada a sido existosa o no.	
 	static char letra;  // Letra a comprobar si existe en palabraADescrubrir .	
 	
 	 
 	public static void main(String[] args) {
+			
+		// Crea una nueva partida. Inicializa todos las variables 
+	    // y pinta la consola con sus valores iniciales.
+		nuevaPartida(); 
 		
-		palabraADescubrir = damePalabra();  // Palabra aleatoria de entre las existentes en PALABRAS.
-		
-		palapraEnConstruccion = new char[palabraADescubrir.length()]; // Se irán añadiendo letras coincidentes suministradas por usuario.
-		
-		letrasUtilizadas = new TreeSet<>();  // Crea colección para guardar letras utilizadas.
-		
-		letra = '_'; // Caracter a guardar en palabraEnConstruccion[i] cuando la letra de la posición i aun no ha sido suministrada.				
-		
-		nuevaPartida(); // Crea una nueva partida.(Pinta horca inicial y patrón (huecos) de la palabra a descubrir ...)
-		
-		for(;;) { 			// Bucle sin condición de parada ni contadores. También se puede poner while (true){
+		for(;;) { 	// Bucle sin condición de parada ni contadores. También se puede poner while (true){
 			
 			letra = pideLetra("Escribe la letra: ");  	// Pide letra por consola.
 			
-			borraConsola();
-			
-			// Comprueba ocurencias de la letra en la palabra a descubrir, las añade a la palabra en construcción, 
-			// pinta la palabra en construcción actualizada y devuelve número de aciertos (ocurrencias de la letra).
-			aciertos = hazJugada();  	
-			
-			// Si letra no está en la palabra a descubrir incrementa contador de fallos.
-			contFallos = aciertos == 0 ? contFallos + 1 : contFallos;  	
-			
-			pintaHorca(contFallos); // Repinta horca según número de fallos.
-			
-			pintaContadoresVidasYTiradas();
-			
-			pintaListaLetrasUsadas();
+			// Comprueba ocurencias de la letra en la palabra a descubrir, las añade a la
+			// palabra en construcción si procede, pinta la palabra en construcción actualizada
+			// borra consola y pinta toda la información actualizada
+			hazJugada();  				
 			
 			// partidaTerminada() evalua si se ha llegado al total de fallos o se ha completado la palabra.
 			// En ambos casos la partida se da por terminada y escribe el mensaje de victoria o derrota por consola,
@@ -140,11 +124,11 @@ public class JuegoAhorcado {
 			// Si no se da ninguna de las condiciones anteriores devuelve false para  seguir pidiendo letras
 			// en la partida en curso			
 			
-			if(partidaTerminada() ) { // Si la partida ha terminado preguntamos si quiere jugar otra
-				// Pregunta por consola si jugar otra partida 
-				if( ! nuevaPartida("\n¿Quieres jugar otra partida? [S/N]: ") ) 
-					break;  	// Si no se quiere jugar otra partida entonces sale del bucle while
-			}	
+			if (partidaTerminada()) { // Si la partida ha terminado preguntamos si quiere jugar otra
+				// Pregunta por consola si jugar otra partida
+				if (!jugarOtraPartida())
+					break; // Si no se quiere jugar otra partida entonces sale del bucle while
+			}
 			
 		} // fin for
 		
@@ -154,52 +138,41 @@ public class JuegoAhorcado {
 	
 	
 	/**
-	 * Pregunta si se quiere jugar otra partida y en caso afirmativo inicializa las 
-	 * varibles palabra, palabraParcial, y pinta por consola la horca inicial y el
-	 * patrón con guiones bajos de la palabra a descubrir
-	 * @return 0 si hemos pulsado S para jugar otra partida; -1 en caso contrario 
-	 */
-	private static boolean nuevaPartida(String t) {
-		if(pideLetra(t) == 'S') {
-			nuevaPartida();
-			return  true;
-		} else return false;
-		
-	}
-	
-	/**
-	 * Crea una nueva partida. Para elo  inicializa las  varibles palabra, palabraParcial, 
-	 * y pinta por consola la horca inicial y el patrón con guiones bajos de la palabra a descubrir
+	 * Crea una nueva partida. Para ello inicializa las varibles palabra,
+	 * palabraParcial, y pinta por consola la horca inicial y el patrón con guiones
+	 * bajos de la palabra a descubrir
 	 */
 	private static void nuevaPartida() {
 		borraConsola();
 		palabraADescubrir = damePalabra();
-		palapraEnConstruccion= new char[palabraADescubrir.length()];
-		letrasUtilizadas.clear();  // Limpia colección de letras utilizadas.
+		palabraEnConstruccion = new char[palabraADescubrir.length()];
+		inicializaPalADescubrir(); // Rellena palabraEnConstrucción con '_'.
+		letrasUtilizadas.clear(); // Limpia colección de letras utilizadas.
 		letra = '_';
-		contFallos = 0;
-		hazJugada();
+		contFallos = contTiradas = 0;
+		pintaPalabraEnConstruccion();
 		pintaHorca(0);
 		pintaContadoresVidasYTiradas();
 	}
 	
 	/**
-	 * Evalua la jugada y escribe por consola mensaje de derrota o victoria en el caso de haber 
-	 * agotado el número de intentos o haber completado la palabra respectivamente.
-	 
-	 * @return true en caso de que la partida haya terminado, bien por derrota o victoria
-	 *         false en caso contrario (la partida continua).
+	 * Evalua la jugada y escribe por consola mensaje de derrota o victoria en el
+	 * caso de haber agotado el número de intentos o haber completado la palabra
+	 * respectivamente.
+	 * 
+	 * @return true en caso de que la partida haya terminado, bien por derrota o
+	 *         victoria. false en caso contrario (la partida continua).
 	 */
 	private static boolean partidaTerminada() {
-		if (aciertos == 0 && contFallos == MAX_FALLOS) {  
+		if (!acierto && contFallos == MAX_FALLOS) {
 			System.out.println("Ooooh, fallaste");
 			System.out.println("La palabra era: " + palabraADescubrir);
 			return true;
-		} else if (String.valueOf(palapraEnConstruccion).equals(palabraADescubrir)) {
+		} else if (String.valueOf(palabraEnConstruccion).equals(palabraADescubrir)) {
 			System.out.println("¡ENHORABUENA, has resuelto la palabra!");
 			return true;
-		} else return false;
-		
+		} else
+			return false;
 
 	}
 	
@@ -210,49 +183,54 @@ public class JuegoAhorcado {
 	 */
 	private static char pideLetra(String t) {
 		System.out.print(t);
-		sc.reset();
-		return  Character.toUpperCase(sc.next().charAt(0));
-		
-		
+		sc.reset(); // Hace reset por si en el buffer de Scanner quedara algo introducido anteriormente.
+		// Si tecleamos por error más de un caracter solo coge el primero.
+		// Convierte el caracter a mayúscula.
+		return  Character.toUpperCase(sc.next().charAt(0)); 
 	}
 	
 	/**
-	 * Añade la letra a palabra, siempre que esta esté presente en PALABRA. La letra
-	 * es añadida en la misma posición que se encuentra en PALABRA. Escribre palabra
-	 * por consola.
+	 * Si la letra esta en palabraADescubrir la incluye en palabraEnConstruccion
+	 * y la añade a letrasUtilizadas.
+	 * Si la letra no está en palabraADescubir incrementa el contador de fallos
+	 * Incrementa contador de tiradas.
+	 * Repinta consola con los nuevos valores: contadores, horca, letras usadas
 	 * 
-	 * Si letra = '_' pinta por consola solo guienes de posición de letras.
-	 * @return número de letras añadidas a palabra 
-	 *         -1 si letra = '_'
-	 *         -1 si la letra ya fue usada en alguna tirada anterior
 	 */
 
-	private static int hazJugada() {
-		int contador = -1;
-		// Pinta por consola solo los guiones de posición de letra.
-		if (letra == '_') {
-			for (int i = 0; i < palapraEnConstruccion.length; i++) 
-				palapraEnConstruccion[i] = '_';			
+	private static  void hazJugada() {
+		boolean acierto = false;
 
-		} else if (!letrasUtilizadas.contains(letra)){ // Si la letra aun no ha sido utilizada
-					
-			// Mete en array palabraEnConstruccion los caracteres coincidentes en
+		// Si la letra ya  ha sido utilizada no hace nada, solo repinta consola 
+		if (!letrasUtilizadas.contains(letra)) {
+
+			// Pone en palabraEnConstruccion los caracteres coincidentes en
 			// palabraADescubrir
-			
-			contador = 0;
-			for (int i = 0; i < palabraADescubrir.length(); i++) {
-				if (palabraADescubrir.charAt(i) == letra) {  // Si la letra está en palabraADescubrir
-					palapraEnConstruccion[i] = letra; // Añade letra al array palabraEnConstruccion.
-					contador++;
-				}				
-			}
-			
-			letrasUtilizadas.add(letra); // Añade letra utilizada a la colección.			
-		} 
-		
-		pintaPalabraEnConstruccion();
-		return contador; // Número de caracteres coincidentes añadidos a palabra.
 
+			for (int i = 0; i < palabraADescubrir.length(); i++) {
+				if (palabraADescubrir.charAt(i) == letra) { // Si la letra está en palabraADescubrir
+					palabraEnConstruccion[i] = letra; // la añade a palabraEnConstruccion
+					acierto = true; // ha habido acierto
+				}
+			}
+
+			letrasUtilizadas.add(letra); // Añade letra utilizada a la colección.
+			contTiradas++;
+
+			// Si letra no está en la palabra a descubrir incrementa contador de fallos.
+			contFallos = acierto ? contFallos : contFallos + 1;
+		}
+
+		borraConsola();
+		pintaPalabraEnConstruccion();
+		pintaHorca(contFallos); // Repinta horca según número de fallos.
+		pintaContadoresVidasYTiradas();
+		pintaListaLetrasUsadas();
+	}
+	
+	static private void inicializaPalADescubrir() {
+		for (int i = 0; i < palabraEnConstruccion.length; i++)
+			palabraEnConstruccion[i] = '_';
 	}
 	
 	private static void pintaContadoresVidasYTiradas() {
@@ -260,8 +238,8 @@ public class JuegoAhorcado {
 	}
 	
 	private static void  pintaPalabraEnConstruccion() {
-		for (int i = 0; i < palapraEnConstruccion.length; i++) 
-			System.out.printf("%s ", palapraEnConstruccion[i]);
+		for (int i = 0; i < palabraEnConstruccion.length; i++) 
+			System.out.printf("%s ", palabraEnConstruccion[i]);
 		System.out.println("\n");
 
 	}
@@ -275,13 +253,6 @@ public class JuegoAhorcado {
 	 * Escribe 200 líneas en blanco para simular un borrado de consola
 	 */
 	private static void borraConsola() {
-		
-		//Versión para borrar consola MS-DOS
-		/*try {
-			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-		} catch (InterruptedException | IOException e) {
-			e.printStackTrace();
-		}*/
 		
 		// Para consola Eclipse. Simula borrado metiendo líneas en blanco.
 		for(int i=0; i< 200; i++) {
@@ -307,12 +278,25 @@ public class JuegoAhorcado {
 	}
 	
 	/**
+	 * Pregunta si se quiere jugar otra partida y en caso afirmativo inicializa las
+	 * varibles palabra, palabraParcial, y pinta por consola la horca inicial y el
+	 * patrón con guiones bajos de la palabra a descubrir
+	 * 
+	 * @return 0 si hemos pulsado S para jugar otra partida; -1 en caso contrario
+	 */
+	private static boolean jugarOtraPartida() {
+		if (pideLetra("\n¿Quieres jugar otra partida? [S/N]: ") == 'S') {
+			nuevaPartida();
+			return true;
+		} else
+			return false;
+	}
+	
+	/**
 	 * Pinta por consola la lista de letras que ya han sido utilizadas
 	 */
 	private static void pintaListaLetrasUsadas() {
 		System.out.print("Letras usadas: ");
 		System.out.println(letrasUtilizadas.toString());
 	}
-	
-
 }
